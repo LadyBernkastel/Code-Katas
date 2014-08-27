a6a26095cd20f57250aa3a6a52bf6ef45a7dba2f
LadyBernkastel <Rachael@bernkastel.co.uk>
2014-08-27 21:44:52 +0100

    Initial Code Kata 2 commit

---
+++ CodeKata 2 - Binary Chop/src/Main.java
@@ -0,0 +1,63 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class Main {
+	@Test
+	public void testChop() {
+		int[] emptyData = {};
+	    assertEquals(-1, chop(3, emptyData));
+	    int[] singleData = {1};
+	    assertEquals(-1, chop(3, singleData));
+	    assertEquals(0, chop(1, singleData));
+	    int[] threeData = {1, 3, 5};
+	    assertEquals(0, chop(1, threeData));
+	    assertEquals(1, chop(3, threeData));
+	    assertEquals(2, chop(5, threeData));
+		assertEquals(-1, chop(0, threeData));
+		assertEquals(-1, chop(2, threeData));
+		assertEquals(-1, chop(4, threeData));
+		assertEquals(-1, chop(6, threeData));
+		int[] fourData = {1, 3, 5, 7};
+		assertEquals(0, chop(1, fourData));
+		assertEquals(1, chop(3, fourData));
+		assertEquals(2, chop(5, fourData));
+		assertEquals(3, chop(7, fourData));
+		assertEquals(-1, chop(0, fourData));
+		assertEquals(-1, chop(2, fourData));
+		assertEquals(-1, chop(4, fourData));
+		assertEquals(-1, chop(6, fourData));
+		assertEquals(-1, chop(8, fourData));
+	}
+	
+	private int chop(int target, int[] dataSet) {
+		int arrayLength = dataSet.length;
+		
+		if(arrayLength == 0) {
+			return -1;
+		}
+		
+		int arrayMiddle = arrayLength/2;
+		int middleValue = dataSet[arrayMiddle];
+		
+		if (arrayLength == 1 && middleValue != target) {
+			return -1;
+		}
+		
+		if (middleValue > target) {
+			int choppedLength = arrayMiddle;
+			int[] choppedArray = new int[choppedLength];
+			System.arraycopy(dataSet, 0, choppedArray, 0, choppedLength);
+			return chop(target, choppedArray);
+		} else if (middleValue < target) {
+			int choppedLength = arrayLength-arrayMiddle;
+			int[] choppedArray = new int[choppedLength];
+			System.arraycopy(dataSet, arrayMiddle, choppedArray, 0, choppedLength);
+			return chop(target, choppedArray);			
+		}
+		
+		return arrayMiddle;
+	}
+
+}
